88dddb2b5922a8e6ae25ba446c3acd1c45beeb5b
Ben Davenport-Ray <laserdude11@gmail.com>
2012-06-08 14:23:24 -0400

    Added TaskList object.

---
+++ include/tasklist.h
@@ -0,0 +1,19 @@
+#ifndef TaskList
+#define TaskList
+
+#include "task.h"
+
+typedef struct tasklist_t {
+    TaskList *next;
+    Task* this;
+}TaskList;
+
+TaskList* TaskList_new();
+void TaskList_delete(TaskList *list);
+
+int TaskList_append(TaskList *list, Task *task);
+TaskList* TaskList_filter(TaskList *list, char *string);
+
+
+
+#endif
---
+++ src/tasklist.c
@@ -0,0 +1,8 @@
+#include "tasklist.h"
+
+TaskList* TaskList_new(){
+}
+
+void TaskList_free(TaskList* list){
+    
+}
