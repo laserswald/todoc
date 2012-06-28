2bce89d7ac837f4a09d61191d2f4235be2d0ee56
Ben Davenport-Ray <ben.davenportray@gmail.com>
2012-06-28 00:52:06 -0400

    Fixed some debugging things.

--- include/debug.h
+++ include/debug.h
@@ -1,8 +1,10 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#ifdef DEBUG
-#define debugprint(M, ...) printf("Debug: %s %d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#ifdef NDEBUG
+#define debugprint(M, ...) 
 #else 
-#define debugprint()
+#define debugprint(M, ...) printf("Debug: %s %d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#endif
+
 #endif
