2f01235c22339933e361633095df428dd061cdaf
Ben Davenport-Ray <ben.davenportray@gmail.com>
2012-08-04 12:14:42 -0400

    Added removing tasks to the object.

--- src/tasklist.c
+++ src/tasklist.c
@@ -17,6 +17,17 @@ void tasklist_free(struct tasklist_t* list){
     tasklist_free(list->next); 
 }
 
+struct tasklist_t* tl_move_to(struct tasklist_t* list, int index)
+{
+    struct tasklist_t* point = list;
+    int ind_pt = 0;
+    while (point && ind_pt < index){
+        point = point->next;
+        ind_pt++;
+    }
+    return point; 
+}
+
 int tasklist_append(struct tasklist_t* this, struct task_t* t){
     if (this == NULL){
         // Make sure it's heard.
@@ -39,9 +50,12 @@ int tasklist_append(struct tasklist_t* this, struct task_t* t){
 
 struct tasklist_t* tasklist_filter(struct tasklist_t* list, char* filter){
     if (!list || !list->task && !list->next){
-        puts("Empty tasklist.")
+        puts("Empty tasklist.");
         return NULL;
     }
+    struct tasklist_t *iter = list;
+    while(iter != NULL){
+
 }
 
 void tasklist_dump(struct tasklist_t *list, char* filename)
@@ -81,3 +95,14 @@ void tasklist_read(struct tasklist_t *list, char* filename){
         iter = iter->next;
     }
 }
+
+void tasklist_rm(struct tasklist_t *list, int index)
+{
+    struct tasklist_t *prev, that;
+    prev = tl_move_to(list, index-1);
+    that = prev->next;
+    prev->next = that->next;
+    that->next = NULL;
+    tasklist_free(that);
+}
+
